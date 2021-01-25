d100258be8ba7967f074aa852623c24b72afc936
Jean Leonel <jean.leonel.costa@gmail.com>
2021-01-25 12:05:16 -0300

    Configurando validadores e mensagens customizadas

---
+++ src/br/com/jcacademy/application/util/ValidationException.java
@@ -0,0 +1,30 @@
+package br.com.jcacademy.application.util;
+
+public class ValidationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException() {
+
+	}
+
+	public ValidationException(String message) {
+		super(message);
+
+	}
+
+	public ValidationException(Throwable cause) {
+		super(cause);
+
+	}
+
+	public ValidationException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+
+	}
+
+}
